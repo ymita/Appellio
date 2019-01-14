a50f3028105a6e7397156e2366efca2f899dc454
ymita <ymita0617@gmail.com>
2019-01-14 20:31:25 +0900

    Password related labels

--- Appellio/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ Appellio/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -35,17 +35,17 @@ namespace Appellio.Areas.Identity.Pages.Account.Manage
         {
             [Required]
             [DataType(DataType.Password)]
-            [Display(Name = "Current password")]
+            [Display(Name = "現在のパスワード")]
             public string OldPassword { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "New password")]
+            [Display(Name = "新しいパスワード")]
             public string NewPassword { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
+            [Display(Name = "(確認)新しいパスワード")]
             [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
         }
